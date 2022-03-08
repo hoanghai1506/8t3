f36565fe9f5282c790bb27a081668abde3eebd8c
hoanghai1506 <92379428+hoanghai1506@users.noreply.github.com>
2022-03-08 08:03:27 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+var i=1;
+
+$('#start').click(function() {
+    alert('Happy womans day! Người bạn xinh đẹp của tớ!<3');
+    $('#start').hide();
+
+
+    $('#them').html('<source src="./images/sound.mp3" type="audio/mp3">');
+
+    setInterval(function(){
+        i++;
+        if (i%2==0) {
+            document.getElementById('show1').src = "./images/"+i+".jpg";
+            $('#show1').fadeIn(1000);
+            $('#show2').fadeOut(1000);
+        } else {
+            document.getElementById('show2').src = "./images/"+i+".jpg";
+            $('#show2').fadeIn(1000);
+            $('#show1').fadeOut(1000);
+        }
+        if (i>6) {
+            i=1;
+        }
+    }, 3000);
+});
+
+
+
+
